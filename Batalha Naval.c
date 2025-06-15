189b01c4bf292e2cf6e698c8ff809ceb5f4f6196
SimiTeste <simiguilherme45@gmail.com>
2025-06-15 11:32:12 -0300

    Add files via upload
    Jogo de Batalha Naval

---
+++ Batalha Naval.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAM_TABULEIRO 10
+#define TAM_HABILIDADE 5
+#define VALOR_AGUA 0
+#define VALOR_NAVIO 3
+#define VALOR_HABILIDADE 5
+
+void inicializarTabuleiro(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO]) {
+    for (int i = 0; i < TAM_TABULEIRO; i++) {
+        for (int j = 0; j < TAM_TABULEIRO; j++) {
+            tabuleiro[i][j] = VALOR_AGUA;
+        }
+    }
+    tabuleiro[2][2] = VALOR_NAVIO;
+    tabuleiro[2][3] = VALOR_NAVIO;
+    tabuleiro[3][2] = VALOR_NAVIO;
+    tabuleiro[5][5] = VALOR_NAVIO;
+}
+
+void exibirTabuleiro(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO]) {
+    for (int i = 0; i < TAM_TABULEIRO; i++) {
+        for (int j = 0; j < TAM_TABULEIRO; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void gerarCone(int matriz[TAM_HABILIDADE][TAM_HABILIDADE]) {
+    int centro = TAM_HABILIDADE / 2;
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            if (i >= abs(j - centro)) {
+                matriz[i][j] = 1;
+            } else {
+                matriz[i][j] = 0;
+            }
+        }
+    }
+}
+
+void gerarCruz(int matriz[TAM_HABILIDADE][TAM_HABILIDADE]) {
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            if (i == TAM_HABILIDADE / 2 || j == TAM_HABILIDADE / 2) {
+                matriz[i][j] = 1;
+            } else {
+                matriz[i][j] = 0;
+            }
+        }
+    }
+}
+
+void gerarOctaedro(int matriz[TAM_HABILIDADE][TAM_HABILIDADE]) {
+    int centro = TAM_HABILIDADE / 2;
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            if (abs(i - centro) + abs(j - centro) <= centro) {
+                matriz[i][j] = 1;
+            } else {
+                matriz[i][j] = 0;
+            }
+        }
+    }
+}
+
+void aplicarHabilidade(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO],
+                       int matriz[TAM_HABILIDADE][TAM_HABILIDADE],
+                       int origemLinha, int origemColuna) {
+    int offset = TAM_HABILIDADE / 2;
+    for (int i = 0; i < TAM_HABILIDADE; i++) {
+        for (int j = 0; j < TAM_HABILIDADE; j++) {
+            if (matriz[i][j] == 1) {
+                int linhaTab = origemLinha + (i - offset);
+                int colunaTab = origemColuna + (j - offset);
+                if (linhaTab >= 0 && linhaTab < TAM_TABULEIRO &&
+                    colunaTab >= 0 && colunaTab < TAM_TABULEIRO) {
+                    if (tabuleiro[linhaTab][colunaTab] != VALOR_NAVIO) {
+                        tabuleiro[linhaTab][colunaTab] = VALOR_HABILIDADE;
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO];
+    int cone[TAM_HABILIDADE][TAM_HABILIDADE];
+    int cruz[TAM_HABILIDADE][TAM_HABILIDADE];
+    int octaedro[TAM_HABILIDADE][TAM_HABILIDADE];
+
+    inicializarTabuleiro(tabuleiro);
+    gerarCone(cone);
+    gerarCruz(cruz);
+    gerarOctaedro(octaedro);
+
+    printf("Tabuleiro original com navios:\n");
+    exibirTabuleiro(tabuleiro);
+
+    aplicarHabilidade(tabuleiro, cone, 1, 2);
+    aplicarHabilidade(tabuleiro, cruz, 5, 5);
+    aplicarHabilidade(tabuleiro, octaedro, 8, 8);
+
+    printf("Tabuleiro com habilidades aplicadas:\n");
+    exibirTabuleiro(tabuleiro);
+
+    return 0;
+}
